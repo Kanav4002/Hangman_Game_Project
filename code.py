e5c3d932189f4bebf52e12cb00b528d1e1281c01
Harman Kaur <harmankaur0779@gmail.com>
2024-03-19 09:55:45 +0530

    Version 2.0

--- code.py
+++ code.py
@@ -1,132 +1,130 @@
 import random
+import os
+import time
+from threading import Thread
+from colorama import Fore, Style
 
-HANGMAN_PICS = ['''
-    +---+
-         |
-         |
-         |
-        ===''', '''
-    +---+
-    O   |
-        |
-        |
-       ===''', '''
-    +---+
-    O   |
-    |   |
-        |
-       ===''', '''
-    +---+
-    O   |
-   /|   |
-        |
-       ===''', '''
-    +---+
-    O   |
-   /|\  |
-        |
-       ===''', '''
-    +---+
-    O   |
-   /|\  |
-   /    |
-       ===''', '''
-    +---+
-    O   |
-   /|\  |
-   / \  |
-       ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+efecnblt = ['''
+      
+_________            .___               _____  ________          __          
+\_   ___ \  ____   __| _/____     _____/ ____\ \______ \  __ ___/  |_ ___.__.
+/    \  \/ /  _ \ / __ |/ __ \   /  _ \   __\   |    |  \|  |  \   __<   |  |
+\     \___(  <_> ) /_/ \  ___/  (  <_> )  |     |    `   \  |  /|  |  \___  |
+ \______  /\____/\____ |\___  >  \____/|__|    /_______  /____/ |__|  / ____|
+        \/            \/    \/                         \/             \/     
+''']
 
-def getRandomWord(wordList):
-    """
-    Returns a random string from the passed list of strings.
-    """
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+hangman = ['''
+   +---+
+       |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+   |   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  /    |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  / \  |
+      ===''']
 
-def displayBoard(missedLetters, correctLetters, secretWord):
+word_list = ["abruptly","absurd","boxful","cycle","duplex","exodus",
+             "funny","gnostic","hyphen","injury","jumbo","knapsack",
+             "luxury","mystify","nightclub","oxygen","puzzling",
+             "quiz","rhythm","subway","transplant","unzip",
+             "vortex","wizard","yummy","zigzag"]
+chosen_word = random.choice(word_list)
+
+os.system("clear")
+
+# Function to print ASCII art with animation effect
+def print_animation(art_list, delay=0.1):
+    for frame in art_list:
+        print(frame)
+        time.sleep(delay)
+
+# Timer function
+def timer(seconds):
+    print(Fore.YELLOW + "\nStarting game in:")
+    for i in range(seconds, 0, -1):
+        print(Fore.YELLOW + f"{i} ", end="", flush=True)
+        time.sleep(1)
     print()
-    print(HANGMAN_PICS[len(missedLetters)])
 
-    print()
-    print('Missed letters: ', end=' ')
-    for letter in missedLetters:
-        print(letter, end=' ')
+# Starting the timer for 3 seconds
+timer_thread = Thread(target=timer, args=(3,))
+timer_thread.start()
 
-    print()
-    blanks = '_' * len(secretWord)
-    for i in range(len(secretWord)):
-        if secretWord[i] in correctLetters:
-            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
-    # Display the secret word with spaces between the letters:
-    for letter in blanks:
-        print(letter, end =' ')
-    print()
+# Printing ASCII art with animation after timer
+timer_thread.join()
+print_animation(efecnblt)
 
-def getGuess(alreadyGuessed):
-    """
-    Returns the letter the player entered.
-    Ensures the player enters a single letter and nothing else.
-    """
-    while True:
-        print('Please guess a letter.')
-        guess = input()
-        guess = guess.lower()
-        if len(guess) != 1:
-            print('Only a single letter is allowed.')
-        elif guess in alreadyGuessed:
-            print('You have already guessed that letter. Choose again.')
-        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-            print('Please enter a letter from the alphabet.')
-        else:
-            return guess
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+print(Fore.CYAN + "\n" + " ".join(alphabet) + "\n")
 
-def playAgain():
-    """
-    Returns True if the player wants to play again, False otherwise.
-    """
-    print('Would you like to play again? (y)es or (n)o')
-    return input().lower().startswith('y')
+screen = ["_"] * len(chosen_word)
+print(Fore.BLUE + f"\t\t{' '.join(screen)}" + Style.RESET_ALL)
+print(Fore.CYAN + hangman[0])
 
-print('|_H_A_N_G_M_A_N_|')
-missedLetters = ''
-correctLetters = ''
-secretWord = getRandomWord(words)
-gameIsDone = False
+guessed = []
+counter = 6
 
-# Now for the game itself:
 while True:
-    displayBoard(missedLetters, correctLetters, secretWord)
-    # Let the player enter a letter:
-    guess = getGuess(missedLetters + correctLetters)
+    guess_a_word = input("\nGuess a word: ").lower()
+    os.system("clear")
 
-    if guess in secretWord:
-        correctLetters = correctLetters + guess
-        # Check to see if the player has won:
-        foundAllLetters = True
-        for i in range(len(secretWord)):
-            if secretWord[i] not in correctLetters:
-                foundAllLetters = False
-                break
-        if foundAllLetters:
-            print('You guessed it!')
-            print('The secret word is "' + secretWord + '"! You win!')
-            gameIsDone = True
+    if guess_a_word in guessed:
+        print(Fore.RED + f"\t\tYou already guessed '{guess_a_word}'!" + Style.RESET_ALL)
+    elif guess_a_word not in guessed:
+        if guess_a_word not in chosen_word:
+            counter -= 1
+        guessed.append(guess_a_word)
+
+    for i, letter_in_word in enumerate(chosen_word):
+        if guess_a_word == letter_in_word:
+            screen[i] = guess_a_word
+
+    for i, letter_in_alphabet in enumerate(alphabet):
+        if guess_a_word == letter_in_alphabet:
+            alphabet[i] = "-"
+
+    guessed.append(guess_a_word)
+
+    print(Fore.GREEN + f"\n{' '.join(efecnblt)}\n")
+    print(Fore.CYAN + " ".join(alphabet) + "\n")
+    print(Fore.BLUE + f"\t\t{' '.join(screen)}" + Style.RESET_ALL)
+
+    if counter > 0:
+        print(Fore.CYAN + hangman[6 - counter])
     else:
-        missedLetters = missedLetters + guess
+        print(Fore.CYAN + hangman[6])
+        print(Fore.RED + "\n\n-------YOU LOST!--------\n")
+        print(Fore.RED + f"The answer was --->  {chosen_word}\n" + Style.RESET_ALL)
+        break
 
-        # Check if the player has guessed too many times and lost.
-        if len(missedLetters) == len(HANGMAN_PICS) -1:
-            displayBoard(missedLetters, correctLetters, secretWord)
-            print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
-            gameIsDone = True
-    # If the game is done, ask the player to try again.
-    if gameIsDone:
-        if playAgain():
-            missedLetters = ''
-            correctLetters = ''
-            gameIsDone = False
-            secretWord = getRandomWord(words)
-        else:
-            break
+    if "_" not in screen:
+        print(Fore.MAGENTA + "\n\nCongratulations!\nYOU WON THE GAME :)\n" + Style.RESET_ALL)
+        break
